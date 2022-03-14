9105b2e2f8fb84dc857e296370d7be444695f32a
zolori <44702305+zolori@users.noreply.github.com>
2022-03-14 16:04:40 +0100

    shaders jour 5

--- jour5.cpp
+++ jour5.cpp
@@ -26,7 +26,15 @@ void triangle()
         SDL_Event currEvent;
         while (SDL_PollEvent(&currEvent)) {}
 
-        initClear();
+        glViewport(0, 0, 1024, 768);
+        glClearColor(0.0, 0.0, 0.0, 0.0);
+        glClearDepth(1.0);
+        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+        glEnable(GL_DEPTH_TEST);
+        glDepthMask(GL_TRUE);
+        glDepthFunc(GL_LEQUAL);
+        //glEnable(GL_LIGHTING);
+        //glEnable(GL_LIGHT0);
 
 		// Use our shader
 		glUseProgram(programID);
@@ -38,7 +46,23 @@ void triangle()
            0.0f,  1.0f, 0.0f,
         };
          
-        initBuffer(g_vertex_buffer_data);
+        GLuint vertexbuffer;
+        glGenBuffers(1, &vertexbuffer);
+        glBindBuffer(GL_ARRAY_BUFFER, vertexbuffer);
+        glBufferData(GL_ARRAY_BUFFER, sizeof(g_vertex_buffer_data), g_vertex_buffer_data, GL_STATIC_DRAW);
+
+
+        // 1rst attribute buffer : vertices
+        glEnableVertexAttribArray(0);
+        glBindBuffer(GL_ARRAY_BUFFER, vertexbuffer);
+        glVertexAttribPointer(
+            0,                  // attribute 0. No particular reason for 0, but must match the layout in the shader.
+            3,                  // size
+            GL_FLOAT,           // type
+            GL_FALSE,           // normalized?
+            0,                  // stride
+            (void*)0            // array buffer offset
+        );
 
         // Draw the triangle !
         glDrawArrays(GL_TRIANGLES, 0, 3); // Starting from vertex 0; 3 vertices total -> 1 triangle
