fbcbd274f72cf21bd4efddb3a2e5db1ea4b9fc46
Arthur Bouffay <80390248+Tempauras@users.noreply.github.com>
2022-03-28 09:34:48 +0200

    fix sphere

--- engineObjects/CoreClasses/SolidSphere.cpp
+++ engineObjects/CoreClasses/SolidSphere.cpp
@@ -9,39 +9,39 @@ SolidSphere::SolidSphere(float radius, unsigned int rings, unsigned int sectors)
     vertices.resize(rings * sectors * 3);
     normals.resize(rings * sectors * 3);
     texcoords.resize(rings * sectors * 2);
-std::vector<GLfloat>::iterator v = vertices.begin();
-        std::vector<GLfloat>::iterator n = normals.begin();
-        std::vector<GLfloat>::iterator t = texcoords.begin();
-        for(r = 0; r < rings; r++) for(s = 0; s < sectors; s++) {
-                float const y = sin( -M_PI_2 + M_PI * r * R );
-                float const x = cos(2*M_PI * s * S) * sin( M_PI * r * R );
-                float const z = sin(2*M_PI * s * S) * sin( M_PI * r * R );
+    std::vector<GLfloat>::iterator v = vertices.begin();
+    std::vector<GLfloat>::iterator n = normals.begin();
+    std::vector<GLfloat>::iterator t = texcoords.begin();
+    for (r = 0; r < rings; r++) for (s = 0; s < sectors; s++) {
+        float const y = sin(-M_PI_2 + M_PI * r * R);
+        float const x = cos(2 * M_PI * s * S) * sin(M_PI * r * R);
+        float const z = sin(2 * M_PI * s * S) * sin(M_PI * r * R);
 
-                *t++ = s*S;
-                *t++ = r*R;
+        *t++ = s * S;
+        *t++ = r * R;
 
-                *v++ = x * radius;
-                *v++ = y * radius;
-                *v++ = z * radius;
+        *v++ = x * radius;
+        *v++ = y * radius;
+        *v++ = z * radius;
 
-                *n++ = x;
-                *n++ = y;
-                *n++ = z;
-        }
+        *n++ = x;
+        *n++ = y;
+        *n++ = z;
+    }
 
     indices.resize(rings * sectors * 4);
     std::vector<unsigned int>::iterator i = indices.begin();
     for (r = 0; r < rings; r++) for (s = 0; s < sectors; s++) {
         *i++ = r * sectors + s;
         *i++ = r * sectors + (s + 1);
-        *i++ = (r + 1) * sectors + s;
         *i++ = (r + 1) * sectors + (s + 1);
+        *i++ = (r + 1) * sectors + s;
     }
 }
 
 void SolidSphere::Draw()
 {
-    Buffer* VerticesBuffer = new Buffer(vertices, 0, 4);
+    Buffer* VerticesBuffer = new Buffer(vertices, 0, 3);
     VerticesBuffer->BindBuffer();
     Buffer* NormalBuffer = new Buffer(normals, 1, 3);
     NormalBuffer->BindBuffer();
