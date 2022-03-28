4585cec3c40e4f666a04347cbaff6b5be4fb9533
zolori <44702305+zolori@users.noreply.github.com>
2022-03-28 16:50:20 +0200

    meshs

--- main.cpp
+++ main.cpp
@@ -234,7 +234,7 @@ int main(int argc, char* argv[])
         duration<float> elapsedSeconds = curTime - prevTime;
 
         ImGui::Begin("Perfs");
-        //ImGui::LabelText("Frame Time (ms) : ", "%f", elapsedSeconds.count() * 1e-3);
+        ImGui::LabelText("Frame Time (ms) : ", "%f", elapsedSeconds.count() * 1e-3);
         ImGui::LabelText("Triangles : ", "%d", var);
         ImGui::LabelText("FPS : ", "%f", 1.0 / elapsedSeconds.count());
         ImGui::End();
