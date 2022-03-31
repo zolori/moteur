05078ea0ada05133e52ffeb649cc3be35f42e55f
zolori <44702305+zolori@users.noreply.github.com>
2022-03-31 16:10:51 +0200

    able/disable balls

--- main.cpp
+++ main.cpp
@@ -22,7 +22,7 @@
 
 using namespace std;
 using namespace glm;
-   
+
 
 SDL_Window* SetUpWindow()
 {
@@ -56,38 +56,38 @@ int main(int argc, char* argv[])
 	ImGuiIO& io = initApp(win);
 	glewInit();
 
-    std::vector<Light*> objectLights;
-    Light* light1 = new Light("light1", vec3(-10.0, 5.0, 2.0), vec3(1.0, 0.0, 1.0), 100.0f);
-    Light* light2 = new Light("light2", vec3(0.0, 5.0, 2.0), vec3(0.5, 1.0, 1.0), 50.0f);
-    Light* light3 = new Light("light3", vec3(-2.0, 4.0, 2.0), vec3(0.25, 0.75, 0), 60.0f);
-    Light* light4 = new Light("light4", vec3(2, 5, 2), vec3(0.15, 1, 0.65), 75.0f);
-    Light* light5 = new Light("light5", vec3(-2, 5, 2), vec3(1, .45, 0.35), 42.0f);
-    Light* light6 = new Light("light6", vec3(2, 5, 2), vec3(0.5, .56, .89), 37.0f);
-    const char* lights[6]{};
-    objectLights.push_back(light1);
-    objectLights.push_back(light2);
-    objectLights.push_back(light3);
-    objectLights.push_back(light4);
-    objectLights.push_back(light5);
-    objectLights.push_back(light6);
+	std::vector<Light*> objectLights;
+	Light* light1 = new Light("light1", vec3(-10.0, 5.0, 2.0), vec3(1.0, 0.0, 1.0), 100.0f);
+	Light* light2 = new Light("light2", vec3(0.0, 5.0, 2.0), vec3(0.5, 1.0, 1.0), 50.0f);
+	Light* light3 = new Light("light3", vec3(-2.0, 4.0, 2.0), vec3(0.25, 0.75, 0), 60.0f);
+	Light* light4 = new Light("light4", vec3(2, 5, 2), vec3(0.15, 1, 0.65), 75.0f);
+	Light* light5 = new Light("light5", vec3(-2, 5, 2), vec3(1, .45, 0.35), 42.0f);
+	Light* light6 = new Light("light6", vec3(2, 5, 2), vec3(0.5, .56, .89), 37.0f);
+	const char* lights[6]{};
+	objectLights.push_back(light1);
+	objectLights.push_back(light2);
+	objectLights.push_back(light3);
+	objectLights.push_back(light4);
+	objectLights.push_back(light5);
+	objectLights.push_back(light6);
 
-    for (int i = 0; i < objectLights.size(); i++)
-    {
-        lights[i] = objectLights[i]->GetName();
-    }
+	for (int i = 0; i < objectLights.size(); i++)
+	{
+		lights[i] = objectLights[i]->GetName();
+	}
 
-    Program prog1 = Program(SHADER_DIRECTORY, VERTEX_SHADER, FRAGMENT_SHADER, objectLights.size());
-    std::string texture_path_plane = FindFile("assets", "Stone 01_1K_Diffuse.png");
-    std::string texture_path_sphere = FindFile("assets", "Cobblestone_Bump.jpg");
+	Program prog1 = Program(SHADER_DIRECTORY, VERTEX_SHADER, FRAGMENT_SHADER, objectLights.size());
+	std::string texture_path_plane = FindFile("assets", "Stone 01_1K_Diffuse.png");
+	std::string texture_path_sphere = FindFile("assets", "Cobblestone_Bump.jpg");
 
 
-    prog1.AddUniformVar("myTextureSampler");
-    prog1.AddUniformVar("myTextureSampler2");
+	prog1.AddUniformVar("myTextureSampler");
+	prog1.AddUniformVar("myTextureSampler2");
 
-    Texture* tex1 = new Texture();
-    Texture* tex2 = new Texture();
-    tex1->loadIMG(texture_path_plane.c_str());
-    tex2->loadIMG(texture_path_sphere.c_str());
+	Texture* tex1 = new Texture();
+	Texture* tex2 = new Texture();
+	tex1->loadIMG(texture_path_plane.c_str());
+	tex2->loadIMG(texture_path_sphere.c_str());
 
 	auto beginTime = steady_clock::now();
 	auto prevTime = steady_clock::now();
@@ -104,19 +104,20 @@ int main(int argc, char* argv[])
 	//SDL_ShowCursor(SDL_DISABLE);
 	int var = 0;
 	int nbBall = 0;
+	static bool render = true;
 
 	bool Freelook = false;
 	std::vector<Object*> GameObjects;
 	float sphereRadius = .5f;
 
-    int index = 0;
-    bool isActive = true;
-    float newLightPower = 0.0f;
-    vec3 newLightPosition = vec3(0.0, 0.0, 0.0);
-    float currentLightColor[3]{};
+	int index = 0;
+	bool isActive = true;
+	float newLightPower = 0.0f;
+	vec3 newLightPosition = vec3(0.0, 0.0, 0.0);
+	float currentLightColor[3]{};
 
-    const ImGuiViewport* viewport = ImGui::GetMainViewport();
-    const ImVec2 base_pos = viewport->Pos;
+	const ImGuiViewport* viewport = ImGui::GetMainViewport();
+	const ImVec2 base_pos = viewport->Pos;
 
 	std::random_device rd; // obtain a random number from hardware
 	std::mt19937 gen(rd()); // seed the generator
@@ -126,7 +127,8 @@ int main(int argc, char* argv[])
 
 	BulletPhysics* PhysicsEngine = new BulletPhysics();
 
-	for (size_t i = 0; i < 5001; i++)
+
+	for (size_t i = 0; i < 2001; i++)
 	{
 		if (i == 0)
 		{
@@ -153,7 +155,7 @@ int main(int argc, char* argv[])
 			};
 			VertexAssembly* planeVertexAssembly = new VertexAssembly(Vertices, Vertices, TexCoord, Indices, Vertices);
 			//Create the Mesh
-			Mesh* planeMesh = new Mesh(planeVertexAssembly,tex1);
+			Mesh* planeMesh = new Mesh(planeVertexAssembly, tex1);
 			//Add it to the planeObject
 			planeObject->AddComponent(planeMesh);
 			GameObjects.push_back(planeObject);
@@ -169,12 +171,12 @@ int main(int argc, char* argv[])
 			//Add them to a VertexAssembly
 			VertexAssembly* sphereVertexAssembly = new VertexAssembly(sphereParameter->GetVertices(), sphereParameter->GetNormals(), sphereParameter->GetTexcoords(), sphereParameter->GetIndices(), sphereParameter->GetVertices());
 			//Create the Mesh with the parameter from the VertexAssembly and indices from 
-			Mesh* sphereMesh = new Mesh(sphereVertexAssembly,tex2);
+			Mesh* sphereMesh = new Mesh(sphereVertexAssembly, tex2);
 			//add it to the sphereObject
 			sphereObject->AddComponent(sphereMesh);
 			//Put the sphere object in the vector housing all of them
 			GameObjects.push_back(sphereObject);
-			++nbBall;
+
 		}
 	}
 	while (apprunning)
@@ -220,28 +222,31 @@ int main(int argc, char* argv[])
 					break;
 				case SDLK_SPACE:
 				{
-					//Create a sphere object of name Sphere
-					Object* sphereObject = new Object("Sphere");
-					//Create the sphere in a physic way
-					PhysicsEngine->CreateSphere(sphereRadius,
-						cam.GetPosition().x + cam.GetFront().x,
-						cam.GetPosition().y + cam.GetFront().y,
-						cam.GetPosition().z + cam.GetFront().z,
-						1.0f
-					);
-					btVector3 look = btVector3(cam.GetFront().x, cam.GetFront().y, cam.GetFront().z) * 20;
-					PhysicsEngine->rigidbodies.back()->setLinearVelocity(look);
-					//Create the parameter for the render of the sphere
-					SolidSphere* sphereParameter = new SolidSphere(sphereRadius, 8, 12);
-					//Add them to a VertexAssembly
-					VertexAssembly* sphereVertexAssembly = new VertexAssembly(sphereParameter->GetVertices(), sphereParameter->GetNormals(), sphereParameter->GetTexcoords(), sphereParameter->GetIndices(), sphereParameter->GetVertices());
-					//Create the Mesh with the parameter from the VertexAssembly and indices from 
-					Mesh* sphereMesh = new Mesh(sphereVertexAssembly,tex2);
-					//add it to the sphereObject
-					sphereObject->AddComponent(sphereMesh);
-					//Put the sphere object in the vector housing all of them
-					GameObjects.push_back(sphereObject);
-					++nbBall;
+					if (render)
+					{
+						//Create a sphere object of name Sphere
+						Object* sphereObject = new Object("Sphere");
+						//Create the sphere in a physic way
+						PhysicsEngine->CreateSphere(sphereRadius,
+							cam.GetPosition().x + cam.GetFront().x,
+							cam.GetPosition().y + cam.GetFront().y,
+							cam.GetPosition().z + cam.GetFront().z,
+							1.0f
+						);
+						btVector3 look = btVector3(cam.GetFront().x, cam.GetFront().y, cam.GetFront().z) * 20;
+						PhysicsEngine->rigidbodies.back()->setLinearVelocity(look);
+						//Create the parameter for the render of the sphere
+						SolidSphere* sphereParameter = new SolidSphere(sphereRadius, 8, 12);
+						//Add them to a VertexAssembly
+						VertexAssembly* sphereVertexAssembly = new VertexAssembly(sphereParameter->GetVertices(), sphereParameter->GetNormals(), sphereParameter->GetTexcoords(), sphereParameter->GetIndices(), sphereParameter->GetVertices());
+						//Create the Mesh with the parameter from the VertexAssembly and indices from 
+						Mesh* sphereMesh = new Mesh(sphereVertexAssembly, tex2);
+						//add it to the sphereObject
+						sphereObject->AddComponent(sphereMesh);
+						//Put the sphere object in the vector housing all of them
+						GameObjects.push_back(sphereObject);
+						++nbBall;
+					}
 					break;
 				}
 				default:
@@ -295,26 +300,32 @@ int main(int argc, char* argv[])
 
 		for (size_t i = 0; i < GameObjects.size(); i++)
 		{
-
 			Mesh* MeshComponent = (Mesh*)GameObjects[i]->GetSpecificComponent(ComponentName::MESH_COMPONENT);
 			glm::mat4 Model = glm::mat4(1.0f);
 			glm::quat quaternionRotation = MeshComponent->Rotation(PhysicsEngine->rigidbodies[i]);
 			Model = glm::translate(Model, MeshComponent->Translation(PhysicsEngine->rigidbodies[i]));
 			Model *= mat4(quaternionRotation);
 			glm::mat4 MVP = cam.GetProjection() * cam.GetView() * Model;
-            prog1.UpdateCamera(&MVP[0][0], &Model[0][0], &cam.GetView()[0][0]);
-            if (PhysicsEngine->rigidbodies[i]->getCollisionShape()->getShapeType() == STATIC_PLANE_PROXYTYPE)
+			prog1.UpdateCamera(&MVP[0][0], &Model[0][0], &cam.GetView()[0][0]);
+			if (PhysicsEngine->rigidbodies[i]->getCollisionShape()->getShapeType() == STATIC_PLANE_PROXYTYPE)
 			{
 				var += MeshComponent->DrawPlane();
 			}
 			else
 			{
-				var += MeshComponent->DrawSphere();
+				if (render)
+				{
+					PhysicsEngine->rigidbodies[i]->setActivationState(ACTIVE_TAG);
+					var += MeshComponent->DrawSphere();
+					++nbBall;
+				}
+				else
+					PhysicsEngine->rigidbodies[i]->setActivationState(WANTS_DEACTIVATION);
 			}
 		}
 
-        prog1.Use();
-        prog1.UpdateLights(objectLights);
+		prog1.Use();
+		prog1.UpdateLights(objectLights);
 
 		//Render Loop
 		ImGui_ImplOpenGL3_NewFrame();
@@ -331,50 +342,51 @@ int main(int argc, char* argv[])
 		ImGui::LabelText(" : FPS", "%f", 1.0 / elapsedSeconds.count());
 		ImGui::End();
 
-        //Light Window
-        ImGui::Begin("Lights");
-        ImGui::Combo("Lights", &index, lights, IM_ARRAYSIZE(lights));
-        ImGui::BeginChild("Selected light Parameters", ImVec2(0.0, 0.0), true, ImGuiWindowFlags_None);
-        //Light general info
-        ImGui::Text("%s", lights[index]);
-        isActive = objectLights[index]->GetActive();
-        ImGui::Checkbox("Active", &isActive);
-        objectLights[index]->SetActive(isActive);
+		//Light Window
+		ImGui::Begin("Lights");
+		ImGui::Combo("Lights", &index, lights, IM_ARRAYSIZE(lights));
+		ImGui::BeginChild("Selected light Parameters", ImVec2(0.0, 0.0), true, ImGuiWindowFlags_None);
+		//Light general info
+		ImGui::Text("%s", lights[index]);
+		isActive = objectLights[index]->GetActive();
+		ImGui::Checkbox("Active", &isActive);
+		objectLights[index]->SetActive(isActive);
 
-        //Light Parameters
-        ImGui::Text("Parameters", "");
-        if (isActive)
-        {
-            //Update light position
-            newLightPosition = objectLights[index]->GetLightPosition();
-            ImGui::SliderFloat("Light Position X", &newLightPosition.x, -100.0f, 100.0f);
-            ImGui::SliderFloat("Light Position Y", &newLightPosition.y, -15.0f, 15.0f);
-            ImGui::SliderFloat("Light Position Z", &newLightPosition.z, -15.0f, 15.0f);
-            objectLights[index]->SetLightPosition(newLightPosition);
-            ImGui::Separator();
+		//Light Parameters
+		ImGui::Text("Parameters", "");
+		if (isActive)
+		{
+			//Update light position
+			newLightPosition = objectLights[index]->GetLightPosition();
+			ImGui::SliderFloat("Light Position X", &newLightPosition.x, -100.0f, 100.0f);
+			ImGui::SliderFloat("Light Position Y", &newLightPosition.y, -15.0f, 15.0f);
+			ImGui::SliderFloat("Light Position Z", &newLightPosition.z, -15.0f, 15.0f);
+			objectLights[index]->SetLightPosition(newLightPosition);
+			ImGui::Separator();
 
-            //Update light Color
-            currentLightColor[0] = objectLights[index]->GetLightColor().x;
-            currentLightColor[1] = objectLights[index]->GetLightColor().y;
-            currentLightColor[2] = objectLights[index]->GetLightColor().z;
-            ImGui::ColorEdit3("Light Color", currentLightColor, 0);
-            ImGui::Separator();
-            objectLights[index]->SetLightColor(vec3(currentLightColor[0], currentLightColor[1], currentLightColor[2]));
+			//Update light Color
+			currentLightColor[0] = objectLights[index]->GetLightColor().x;
+			currentLightColor[1] = objectLights[index]->GetLightColor().y;
+			currentLightColor[2] = objectLights[index]->GetLightColor().z;
+			ImGui::ColorEdit3("Light Color", currentLightColor, 0);
+			ImGui::Separator();
+			objectLights[index]->SetLightColor(vec3(currentLightColor[0], currentLightColor[1], currentLightColor[2]));
 
-            //Update light power
-            newLightPower = objectLights[index]->GetLightPower();
-            ImGui::SliderFloat("Light Power", &newLightPower, 1.0f, 100.0f);
-            objectLights[index]->SetLightPower(newLightPower);
+			//Update light power
+			newLightPower = objectLights[index]->GetLightPower();
+			ImGui::SliderFloat("Light Power", &newLightPower, 1.0f, 100.0f);
+			objectLights[index]->SetLightPower(newLightPower);
 
-        }
-        ImGui::EndChild();
-        ImGui::End();
+		}
+		ImGui::EndChild();
+		ImGui::End();
 
 		static float sliderFloat = -10.f;
 		ImGui::Begin("Tools");
 		ImGui::LabelText(" : Triangles", "%d", var);
 		ImGui::LabelText(" : Nb de BOULE", "%d", nbBall);
 		ImGui::SliderFloat("Gravity", &sliderFloat, -50.f, 30.f);
+		ImGui::Checkbox("Enable/disable render balls", &render);
 		ImGui::End();
 
 		PhysicsEngine->SetGravity(sliderFloat);
@@ -386,6 +398,7 @@ int main(int argc, char* argv[])
 		ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
 
 		var = 0;
+		nbBall = 0;
 
 		SDL_GL_SwapWindow(win);
 		PhysicsEngine->Update(elapsedSeconds.count());
